97e759db4f941798ad54cb1903a39bbb68a45bad
Veronica Parra <vero.parragraca@gmail.com>
2024-02-23 09:19:56 +0100

    añadido enunciado primer ejercicio y captura de los test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-//RESUELVE LOS EJERCICIOS AQUI
+//1  Dado el array numbers cuyo valor sea [4, 5, 6, 7, 8, 9, 10],
+//crea una función elevados que devuelva un array con el cuadrado de cada uno de los elementos.
 
 const numbers = [4, 5, 6, 7, 8, 9, 10];
 
